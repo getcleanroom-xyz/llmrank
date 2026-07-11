a32789045bd7a35b4764557e4bc30b56e688575c
OVECJOE <vohachor@gmail.com>
2026-07-11 08:09:47 +0100

    deleted the design-system folder as it is not being used anymore

--- design-system/components/index.ts
+++
@@ -1,8 +0,0 @@
-export { Button, type ButtonProps } from "./components/Button";
-export { Input, type InputProps } from "./components/Input";
-export { Card, type CardProps } from "./components/Card";
-export { Modal, type ModalProps } from "./components/Modal";
-export { Toast, type ToastProps } from "./components/Toast";
-export { Tabs, TabList, Tab, TabPanel } from "./components/Tabs";
-export { Select, type SelectProps } from "./components/Select";
-export { Tooltip, type TooltipProps } from "./components/Tooltip";
